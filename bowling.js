c8150466fc2822b59253543fca179d8314767111
akhnikh <114444837+akhnikh@users.noreply.github.com>
2023-10-18 17:55:41 -0500

    Add files via upload

---
+++ bowling.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("bowling-balls", {
+
+    init: function(){
+       this.bowling();
+    },
+
+    bowling: function(){
+        window.addEventListener("keydown", (e)=> {
+            if (e.key == "b"){
+                
+            }
+        })
+    }
+})
